2463a0726990cf913d5317c4bcece001a0b0ed79
Angela <angelamacmel@gmail.com>
2021-02-09 18:19:25 +0100

    mostrado de tiendas

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
 import {FetchService} from "./servicios/fetchService.js";
-
 const service = new FetchService("http://localhost:8080/EmprInfRs_MachadoAngela/webresourcesAngela/tienda");
 
-service.mostrar().then(tiendas => console.log(tiendas));
+const templateTienda = document.querySelector("#tempTienda");
+const  divTiendas = document.querySelector("#tiendas");
+
+service.mostrar().then(mostrarTiendas);
+
+function mostrarTiendas(tiendas)
+{
+    console.log
+    tiendas.forEach(tienda => {
+        const divTienda = templateTienda.content.cloneNode(true);
+
+        divTienda.querySelector(".nombreTienda").textContent = tienda.nombreTienda;
+
+        divTiendas.appendChild(divTienda);
+        
+    });
+}
--- js/servicios/fetchService.js
+++ js/servicios/fetchService.js
@@ -7,8 +7,14 @@ export class FetchService
 
     mostrar()
     {
-        return fetch(this.urlServidor) //devuelve una promesa
-            .then(respuesta => respuesta.json()) //se ejecuta cuando haya resuelto la promesa
-            .catch(error => console.log("ha ocurrido un error: ", error))
+        return fetch(this.urlServidor) 
+            .then(respuesta => respuesta.json()) 
+    }
+
+    insertar()
+    {
+        fetch(this.urlServidor, {
+            method: 'POST'
+        })
     }
 }
